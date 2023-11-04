42e2a18c8bed413c7f40be0ee4c4f0029eadea3c
nico <pfunes@fi.uba.ar>
2023-11-04 14:41:35 -0300

    add changes due to rubocop lint corrections

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    
+
     it { should validate_presence_of(:price) }
     it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
 
